0c4fef2429f3afff3ef4200d188597941d9b1449
Tao TechIdeas <taotechideas@MacBook-Pro-de-Tao.local>
2023-09-28 18:33:34 -0500

    cambios api

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,15 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+# import sys
+# sys.path.append("..")
 
 api = Blueprint('api', __name__)
+
+from machine_learning import nlp
+from machine_learning import distancias as dis
+
+datos = nlp.importarCSV("https://raw.githubusercontent.com/sebas979/archicosCSV/main/DataSet.csv");
+mT,mK,mA,M=dis.matricesDistancia(datos)
 
 @api.route('/hello', methods=['GET'])
 def hello_world():
@@ -9,3 +18,20 @@ def hello_world():
 @api.route('/echo/<string:message>', methods=['GET'])
 def echo_message(message):
     return jsonify(message=message)
+
+@api.route('/matrices', methods=['POST'])
+def matrices():
+    if request.method == 'POST':
+        # data = request.data
+        # data = request.form['data']
+        # print(request.data)
+
+
+        
+        print(request.form)
+        print('=========================')
+        return True
+    # print(data)
+    # print(type(data))
+    # print('matriz completa')
+    # print(M)
